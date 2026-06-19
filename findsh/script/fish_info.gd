377bcfb27ba9c077b469f85bf01935ad9f283e76
fgiofgio <giossteam1@gmail.com>
2026-06-19 20:59:23 +0800

    fish info outline lag test
    the outline makes it lag on my mac

--- findsh/script/fish_info.gd
+++ findsh/script/fish_info.gd
@@ -1,9 +1,14 @@
 extends Control
 
 @export var current_fish = ""
+var pressed_fish
 
 func _physics_process(delta: float) -> void:
 	if current_fish != "": 
 		$Label.text = current_fish
 		$Label.visible = true 
 	else: $Label.visible = false
+
+
+func _on_label_theme_changed() -> void:
+	pass # Replace with function body.
--- findsh/script/interactablefish.gd
+++ findsh/script/interactablefish.gd
@@ -3,11 +3,16 @@ extends Area3D
 @export var fish_name = "bih fih"
 
 func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
+	$LargeFish.get_active_material(0).next_pass.grow = 0
 
 
 func _on_mouse_entered() -> void:
 	FishInfo.current_fish = fish_name
+	$LargeFish.get_active_material(0).next_pass.grow = true
+	$LargeFish.get_active_material(0).next_pass.grow = 0.03
+
+
+func _on_mouse_exited() -> void:
+	FishInfo.current_fish = ""
+	$LargeFish.get_active_material(0).next_pass.grow = false
+	$LargeFish.get_active_material(0).next_pass.grow = 0.0

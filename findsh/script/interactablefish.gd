56ff6c1ade75ccffda1f06598c78049dc8a5fd13
kentrockcsb <kentrock.rontal@benilde.edu.ph>
2026-06-19 23:13:41 +0800

    Shit

--- findsh/script/interactablefish.gd
+++ findsh/script/interactablefish.gd
@@ -17,12 +17,10 @@ func _on_mouse_entered() -> void:
 	FishInfo.current_fish = fish_name
 	$Mesh.get_active_material(0).next_pass.grow = true
 
-
 func _on_mouse_exited() -> void:
 	if FishInfo.pressed_fish != fish_name:
 		FishInfo.current_fish = ""
 		$Mesh.get_active_material(0).next_pass.grow = false
-
 
 func _on_fish_pressed(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if event is InputEventMouseButton:

9c96365888f820928679ec7abbca2ff4717bbb7a
kentrockcsb <kentrock.rontal@benilde.edu.ph>
2026-06-21 08:47:07 +0800

    Added Lights

--- findsh/script/player.gd
+++ findsh/script/player.gd
@@ -1,13 +1,3 @@
 extends Node3D
-@onready var camera_3d: Camera3D = $"../Camera3D"
-@onready var playercam: Camera3D = $Camera3D
 
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		if camera_3d.current == false:
-			playercam.current = false
-			camera_3d.current = true
-		else:
-			playercam.current = true
-			camera_3d.current = false
-	
+#Hello
